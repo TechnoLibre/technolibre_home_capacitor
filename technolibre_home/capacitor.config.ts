a8140350de23334970f6f7f399ad7f69edfb67c0
Rafaël Huard <rafael.huard@technolibre.ca>
2024-12-31 02:09:43 -0500

    [ADD] technolibre_home

---
+++ technolibre_home/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ca.technolibre.home',
+  appName: 'TechnoLibre Home',
+  webDir: 'dist/technolibre_home/browser'
+};
+
+export default config;
