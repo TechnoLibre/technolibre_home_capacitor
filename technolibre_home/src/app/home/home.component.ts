4e32bb33cd8c9d1fb87334296e819381bf7ba95c
Rafaël Huard <rafael.huard@technolibre.ca>
2025-02-01 21:58:38 -0500

    [ADD] Stored data table in HomeComponent

--- technolibre_home/src/app/home/home.component.ts
+++ technolibre_home/src/app/home/home.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Signal, signal } from '@angular/core';
+import { SecureStoragePlugin } from 'capacitor-secure-storage-plugin';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  usernameData = {
+    isDefined: signal(false),
+    value: signal("")
+  };
+
+  passwordData = {
+    isDefined: signal(false),
+    value: signal("")
+  };
+
+  async ngOnInit() {
+    this.fetchUsernamePassword();
+  }
+
+  private async fetchUsernamePassword() {
+    try {
+      const tempUsername = await SecureStoragePlugin.get({ key: "username" });
+      this.usernameData.isDefined.set(true);
+      this.usernameData.value.set(tempUsername.value);
+    } catch (error) {
+      this.usernameData.isDefined.set(false);
+      this.usernameData.value.set("");
+    }
+
+    try {
+      const tempPassword = await SecureStoragePlugin.get({ key: "password" });
+      this.passwordData.value.set(tempPassword.value);
+    } catch (error) {
+      this.passwordData.value.set("");
+    }
+  }
 
 }
